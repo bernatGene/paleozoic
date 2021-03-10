4986694401b39b1e965da5881222a58ffda7c78e
Bernat  Skrabec <bernat@Bernats-MacBook-Pro.local>
2021-03-10 18:39:47 +0100

    Labyrinth, map maker

---
+++ src/Labyrinth.py
@@ -0,0 +1,50 @@
+import time
+
+import numpy as np
+from assets import tiles
+
+WALL = 0
+FOOD = 127
+NONE = 1
+ASCII_DICT = {WALL: '■', FOOD: '=', NONE: ' '}
+
+
+class Labyrinth:
+    def __init__(self, field_size=64):
+        self.rng = np.random.default_rng(int(time.time()))
+        self.field = np.ones((field_size + 4, field_size + 4), dtype=np.int8)
+        self.field_size = field_size
+        self.build_walls()
+        self.grow_food()
+
+    def reset_seed(self, seed=0):
+        if not seed:
+            seed = int(time.time())
+        self.rng = np.random.default_rng(seed)
+
+    def build_walls(self):
+        fs = self.field_size
+        self.field[0:2, :] = WALL
+        self.field[:, 0:2] = WALL
+        self.field[fs + 2:, :] = WALL
+        self.field[:, fs + 2:] = WALL
+        rows = range(0, self.field_size, 16) # 16 == tile_size
+        cols = range(0, self.field_size, 16)
+        coordinates = [(x0+2, y0+2) for x0 in rows for y0 in cols]
+        for r, c in coordinates:
+            self.field[r:r+16, c:c+16] = tiles.get_random_tile()
+
+    def grow_food(self, food_count=100):
+        rows = self.rng.integers(0, self.field_size + 3, food_count)
+        cols = self.rng.integers(0, self.field_size + 3, food_count)
+        for r, c in zip(rows, cols):
+            if self.field[r][c]:
+                self.field[r][c] = FOOD
+
+    def field_to_string(self):
+        text = ""
+        for row in self.field:
+            for item in row:
+                text += ASCII_DICT[item]
+            text += '\n'
+        return text
---
+++ src/testLabyrinth.py
@@ -0,0 +1,4 @@
+import Labyrinth as lab
+
+l = lab.Labyrinth()
+print(l.field_to_string())
